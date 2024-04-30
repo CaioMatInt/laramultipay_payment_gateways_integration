227a38c77aea3d6bdd6c944c2e9db70fb8cbd231
Caio Matheus <caiointre@gmail.com>
2024-04-30 02:15:08 -0300

    Imports Optimization

--- app/Models/PaymentGatewayTransactionStatus.php
+++ app/Models/PaymentGatewayTransactionStatus.php
@@ -7,7 +7,6 @@ use Illuminate\Database\Eloquent\Model;
 use Illuminate\Database\Eloquent\Relations\BelongsTo;
 use Illuminate\Database\Eloquent\Relations\HasMany;
 use Illuminate\Database\Eloquent\SoftDeletes;
-use App\Models\Transaction;
 
 class PaymentGatewayTransactionStatus extends Model
 {
--- database/seeders/UserTypeSeeder.php
+++ database/seeders/UserTypeSeeder.php
@@ -3,7 +3,6 @@
 namespace Database\Seeders;
 
 use App\Enums\UserType\UserTypeEnum;
-use App\Models\User;
 use App\Models\UserType;
 use Illuminate\Database\Seeder;
 
