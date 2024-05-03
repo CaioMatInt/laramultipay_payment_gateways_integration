cb085491fa8b385dc07742de0a5045b9a296bc5e
Caio Matheus <caiointre@gmail.com>
2024-05-03 02:46:09 -0300

    added comment on migration

--- database/migrations/2024_04_28_134825_create_transactions_table.php
+++ database/migrations/2024_04_28_134825_create_transactions_table.php
@@ -11,6 +11,7 @@ return new class extends Migration {
     public function up(): void
     {
         Schema::create('transactions', function (Blueprint $table) {
+            //@@TODO: migrate to uuid
             $table->id();
             $table->foreignIdFor(Payment::class);
             $table->foreignIdFor(PaymentGatewayTransactionStatus::class);
