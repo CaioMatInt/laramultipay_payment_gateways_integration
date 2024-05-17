24be5420e623e621b37ec9da4874785caf226c91
CaioMatInt <caiointre@gmail.com>
2024-05-17 01:15:44 -0300

    Added PaymentGenericStatus missing tests

--- tests/Unit/Services/PaymentGenericStatusServiceTest.php
+++ tests/Unit/Services/PaymentGenericStatusServiceTest.php
@@ -1,5 +1,6 @@
 <?php
 
+use App\Enums\Payment\PaymentGenericStatusEnum;
 use App\Models\PaymentGenericStatus;
 use App\Services\PaymentGenericStatus\PaymentGenericStatusService;
 use Illuminate\Support\Facades\Cache;
@@ -32,5 +33,29 @@ describe('PaymentGenericStatusService', function () {
         $cachedPaymentGenericStatus = Cache::get(config('cache_keys.payment_generic_status.by_id') . $paymentGenericStatus->id);
         expect($cachedPaymentGenericStatus->id)->toBe($paymentGenericStatus->id)
             ->and($cachedPaymentGenericStatus->name)->toBe($paymentGenericStatus->name);
+    });
+
+    test('can get cached initial status', function () {
+        PaymentGenericStatus::factory()->create([
+            'name' => PaymentGenericStatusEnum::PENDING->value
+        ]);
+
+        $initialStatus = $this->paymentGenericStatusService->getCachedInitialStatus();
+
+        expect($initialStatus->name)->toBe(PaymentGenericStatusEnum::PENDING->value);
+    });
+
+    test('should cache initial status result', function () {
+        PaymentGenericStatus::factory()->create([
+            'name' => PaymentGenericStatusEnum::PENDING->value
+        ]);
+
+        $this->paymentGenericStatusService->getCachedInitialStatus();
+
+        expect(Cache::has(config('cache_keys.payment_generic_status.initial')))
+            ->toBeTrue();
+
+        $cachedInitialStatus = Cache::get(config('cache_keys.payment_generic_status.initial'));
+        expect($cachedInitialStatus->name)->toBe(PaymentGenericStatusEnum::PENDING->value);
     });
 });
